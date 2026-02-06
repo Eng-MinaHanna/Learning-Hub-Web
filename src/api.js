0c6f3e8241e5f3f9052e7a6a12777e79736cd9fa
Eng-MinaHanna <menazzzzz75@gmail.com>
2026-02-06 20:42:58 +0200

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,10 +1,11 @@
 import axios from 'axios';
 
 const API = axios.create({
+    // رابط الباك إند بتاعك على فيرسيل
     baseURL: 'https://learning-hub-et5.vercel.app/api'
 });
 
-// ✅ الكود ده بيضيف التوكن في "راس" كل طلب بتبعه للسيرفر
+// ✅ Interceptor: بيضيف التوكن أوتوماتيك في كل "سلكة" طالعة للسيرفر
 API.interceptors.request.use((config) => {
     const token = localStorage.getItem('ieee_token');
     if (token) {
